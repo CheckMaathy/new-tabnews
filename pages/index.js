79072bce0a1d646d6236e4ec1286300c626e4976
checkmaathy <67769338+CheckMaathy@users.noreply.github.com>
2024-02-13 23:37:35 -0300

    feat: commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
     return <h1>
+        <h3>For my love</h3>
         Nos teus olhos encontro o reflexo do meu amor,<br/>
         no teu sorriso o brilho da minha felicidade,<br/>
         e no teu coração o lar onde sempre pertencerei. Amo-te para além das palavras...
